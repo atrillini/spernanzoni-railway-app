4607031a639481eada9795a106346c08508f0469
atrillini <152901272+atrillini@users.noreply.github.com>
2025-05-21 11:06:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ def import_script():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
 
         # call update stocks
 #update_stocks(cfg)
